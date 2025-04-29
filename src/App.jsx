de49bcfadaa6f6950970baeaec263d9ba386101a
Johanna Larsson <johanna.sk.larsson@outlook.com>
2025-04-29 10:43:52 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-amber-200">
+        hejhej
+      </p>
+    </>
+  )
+}
+
+export default App
