622977c4c7bba80119265f8bc4ffaf0ab0cee170
Johanna Larsson <johanna.sk.larsson@outlook.com>
2025-05-03 21:07:53 +0200

    Add Home content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 function Home() {
   useEffect(() => {
@@ -6,9 +6,15 @@ function Home() {
   }, []);
 
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <div className="flex flex-col items-center justify-center text-center px-4 ">
+      <div className="my-11 md:flex items-center space-x-6 text-nowrap">
+        <img src="images/837111666.jpg" alt="Min bild" className="rounded-full w-96 mb-9 md:mb-0" />
+        <section>
+          <h2 className="text-xl mb-6 text-neutral-800 ">Student | Blivande Frontendutvecklare</h2>
+          <p className="text-base md:text-lg max-w-xl mx-auto mb-10 text-pink-400">Utvecklar min framtid, en rad åt gången.</p>
+        </section>
+      </div>      
+    </div>
   );
 }
 
