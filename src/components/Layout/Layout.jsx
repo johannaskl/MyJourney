05b41f7b9886547783fcac9fd11842622e1cd24a
Johanna Larsson <johanna.sk.larsson@outlook.com>
2025-05-07 18:09:25 +0200

    Update text size and bg

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
     <>
       <header className="text-center mt-10">
         <Link to="/">
-          <h1 className="text-5xl sm:text-6xl md:text-7xl font-semibold mb-10 text-nowrap">Johanna Larsson</h1>
+          <h1 className="text-5xl sm:text-6xl md:text-7xl font-semibold mb-10">Johanna Larsson</h1>
           <hr className="md:max-w-5xl mx-auto my-5" />
         </Link>
 
@@ -25,7 +25,7 @@ const Layout = () => {
             {/* Dropdown för erfarenhet */}
             <li className="relative group mr-1 p-2 rounded-md sm:w-48 hover:bg-stone-700 hover:text-slate-100">
               <Link to="#">ERFARENHET</Link>
-              <ul className="absolute hidden group-hover:block bg-stone-400/40 mt-1 shadow-lg rounded-lg">
+              <ul className="absolute hidden group-hover:block bg-stone-400 mt-1 shadow-lg rounded-lg">
                 <li className={`p-2 rounded-md sm:w-48 ${isActive("/education") ? "bg-stone-700/50 text-slate-100" : "text-neutral-800 hover:bg-stone-700 hover:text-slate-100"}`}>
                   <Link to="/education">UTBILDNING</Link>
                 </li>
