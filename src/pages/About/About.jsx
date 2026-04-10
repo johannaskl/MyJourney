589c6727ae3880b6a008fdb0607a8c703eb9e82c
Johanna Larsson <johanna.sk.larsson@outlook.com>
2026-04-10 22:08:41 +0200

    Various updates in layout and text

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,15 +7,13 @@ function About() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center text-center px-4 ">
-      <div className="my-11 flex flex-col sm:flex-row items-center justify-center sm:space-x-6">
-        <img src={aboutImg} alt="Om mig bild" className="rounded-full w-96 mb-9 md:mb-0" />
-        <section>
-          <h2 className="text-xl font-medium mb-6">Om mig</h2>
-          <p className="text-base md:text-lg max-w-xl mx-auto mb-10">Jag är en 32-åring från Eskilstuna som alltid är nyfiken på att lära mig nya saker. Jag älskar att utmana mig själv och växa, både som person och i min yrkesroll. Just nu fokuserar jag på att utvecklas inom webbutveckling och ser fram emot att ta mig an nya utmaningar.</p>
-        </section>
-      </div>
-    </div>
+    <>
+      <img src={aboutImg} alt="Om mig bild" className="rounded-full w-96 h-96 object-cover mb-9 md:mb-0" />
+      <section className="max-w-md w-full">
+        <h2 className="text-xl font-medium mb-6">Om mig</h2>
+        <p className="text-base md:text-lg max-w-xl mx-auto mb-10">Jag är en 33-åring från Eskilstuna som alltid är nyfiken på att lära mig nya saker. Jag älskar att utmana mig själv och växa, både som person och i min yrkesroll. Just nu fokuserar jag på att utvecklas inom webbutveckling och ser fram emot att ta mig an nya utmaningar.</p>
+      </section>
+    </>
   );
 }
 
--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -7,10 +7,10 @@ function Education() {
 
   return (
     <div className="flex flex-col items-center justify-center text-center px-4">
-      <div className="my-11 flex flex-col md:flex-row items-center justify-center md:space-x-10 lg:space-x-28">
-        <h2 className="text-xl font-medium mb-6 md:mb-0 text-nowrap">Utbildning & kurser</h2>
+      <h2 className="text-xl font-medium my-8">Utbildning & kurser</h2>
+      <div className="my-5">        
 
-        <section className="text-base max-w-xl mx-auto text-left sm:text-justify space-y-4">
+        <section className="text-base max-w-3xl mx-auto text-left space-y-3">
           <div>
             <h3 className="font-semibold">Frontendutveckling – EC Utbildning (distans)</h3>
             <p className="italic text-sm text-pink-400">2024 – pågående</p>
@@ -20,16 +20,13 @@ function Education() {
           </div>
 
           <p>
-            Under min utbildning har jag fått möjlighet att bygga webbapplikationer med HTML, CSS, JavaScript, React, Angular och TypeScript. Jag har arbetat både självständigt och i team för att skapa responsiva och användarvänliga gränssnitt, där jag
-            lärt mig att balansera funktionalitet och design för att ge bästa möjliga användarupplevelse.
+            Under min utbildning har jag fått möjlighet att bygga webbapplikationer med HTML, CSS, JavaScript, React, Angular och TypeScript. Jag har även arbetat med backend-utveckling med fokus på SQL och databashantering, samt fullstack-utveckling med C#, ASP.NET Core, Entity Framework Corre och Web API. Jag har arbetat både självständigt och i team för att skapa responsiva och användarvänliga gränssnitt, där jag lärt mig att balansera funktionalitet och design för att ge bästa möjliga användarupplevelse. Under min LIA-period har jag även arbetat med WordPress och PHP.
           </p>
 
           <div>
-            <h4 className="font-semibold mt-6">Kommande kurser (HT-25, VT-26)</h4>
+            <h4 className="font-semibold mt-6">Kommande kurser</h4>
             <ul className="list-disc list-inside pl-4">
-              <li>Backend-utveckling med JavaScript, SQL, databashantering.</li>
-              <li>Fullstack-fokus med C#, ASP.NET Core, Entity Framework Core och Web API.</li>
-              <li>LIA (Lärande i arbete)</li>
+              <li>LIA (Lärande i arbete) - pågående, Columbird</li>
               <li>Examensarbete</li>
             </ul>
           </div>
